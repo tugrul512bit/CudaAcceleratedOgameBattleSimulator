582979f4d56a1806b7c46d254e027903e728713f
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-07-13 17:09:38 +0300

    Update kernels.cuh

--- kernels.cuh
+++ kernels.cuh
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
+#include <curand_kernel.h>
 namespace Kernels {
-	__global__ void k_initializeShipIndex(uint32_t* numShips, uint32_t* shipIndex_d);
+	__global__ void k_initializeRandomSeeds(curandState* seeds, uint32_t* numShips, uint64_t* baseSeed);
 }
