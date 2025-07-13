afcaae8a360613cb05b735c09f44f9cab65e4639
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-07-13 17:09:16 +0300

    Update main.cu

--- main.cu
+++ main.cu
@@ -1,7 +1,10 @@
-﻿#include "Simulator.cuh"
+#include "Simulator.cuh"
 
 int main() {
     bool debug = true;
-    Simulator simulator(debug);
+    int randomSeed = rand();
+    Simulator simulator(debug, randomSeed);
+    // Adding 1000 ships of type 0.
+    simulator.addShips(1000, 0);
 	return 0;
 }
