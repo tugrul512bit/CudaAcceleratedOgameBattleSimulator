da646ac9101a48a0be9f479144770ebb5091aa58
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-07-20 21:49:15 +0300

    Update kernels.cu

--- kernels.cu
+++ kernels.cu
@@ -14,4 +14,27 @@ namespace Kernels {
 			}
 		}
 	}
+	__global__ void k_initializeShipHulls(uint32_t* numShipsTeam1, uint32_t* numShipsTeam2, SpaceShip* shipsTeam1, SpaceShip* shipsTeam2, uint32_t* defaultShipHulls, uint32_t* numberOfShipTypes) {
+		uint32_t n1 = numShipsTeam1[0];
+		uint32_t n2 = numShipsTeam2[0];
+		uint32_t n = (n1 < n2) ? n2 : n1;
+		uint32_t h = numberOfShipTypes[0];
+		uint32_t threadIndex = threadIdx.x + blockIdx.x * blockDim.x;
+		uint32_t numThreads = blockDim.x * gridDim.x;
+		uint32_t numSteps = (n + numThreads - 1) / numThreads;
+		__shared__ uint32_t defaultHulls[50]; 
+		if (threadIdx.x < h) {
+			defaultHulls[threadIdx.x] = defaultShipHulls[threadIdx.x]; 
+        }
+		__syncthreads();
+		for (uint32_t i = 0; i < numSteps; i++) {
+			uint32_t item = i * numThreads + threadIndex;
+			if (item < n1) {
+				shipsTeam1[item].remainingHull = defaultHulls[shipsTeam1[item].typeIndex]; 
+			}
+			if (item < n2) {
+				shipsTeam2[item].remainingHull = defaultHulls[shipsTeam2[item].typeIndex];
+			}
+		}
+	}
 }
