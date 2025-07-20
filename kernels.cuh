249f27378cbff36fdb4350e0cc5e98c3af944529
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-07-20 21:48:53 +0300

    Update kernels.cuh

--- kernels.cuh
+++ kernels.cuh
@@ -3,6 +3,12 @@
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 #include <curand_kernel.h>
+struct SpaceShip {
+	uint32_t typeIndex;
+	uint32_t remainingHull;
+	uint32_t targetIndex;
+};
 namespace Kernels {
 	__global__ void k_initializeRandomSeeds(curandState* seeds, uint32_t* numShips, uint64_t* baseSeed);
+	__global__ void k_initializeShipHulls(uint32_t* numShipsTeam1, uint32_t* numShipsTeam2, SpaceShip* shipsTeam1, SpaceShip* shipsTeam2, uint32_t* defaultShipHulls, uint32_t* numberOfShipTypes);
 }
