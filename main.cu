ee17b61f15ba9efddb403558d4ae79e9ee22ebae
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-07-20 21:48:08 +0300

    Update main.cu

--- main.cu
+++ main.cu
@@ -4,7 +4,6 @@ int main() {
     bool debug = true;
     int randomSeed = rand();
     Simulator simulator(debug, randomSeed);
-    // Adding 1000 ships of type 0.
-    simulator.addShips(1000, 0);
-	return 0;
+    simulator.demo();
+    return 0;
 }
