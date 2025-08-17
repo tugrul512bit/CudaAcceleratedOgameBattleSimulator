1fb254e6df9be7b93bc6e531a3c52570204021d3
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-08-17 21:04:49 +0300

    Update main.cu

--- main.cu
+++ main.cu
@@ -3,7 +3,23 @@
 int main() {
     bool debug = true;
     int randomSeed = rand();
-    Simulator simulator(debug, randomSeed);
-    simulator.demo();
+    int randomSeed2 = rand();
+    int lightFighter = 0;
+    int heavyFighter = 1;
+    std::vector<SpecShipBlockDescriptor> fleet1;
+    std::vector<SpecShipBlockDescriptor> fleet2;
+    SpecShipBlockDescriptor lFighter;
+    lFighter.count = 900000;
+    lFighter.typeIndex = lightFighter;
+    SpecShipBlockDescriptor hFighter;
+    hFighter.count = 100000;
+    hFighter.typeIndex = heavyFighter;
+    fleet1.push_back(lFighter);
+    fleet1.push_back(hFighter);
+    fleet2.push_back(lFighter);
+    fleet2.push_back(hFighter);
+    int gpuIndex = 0;
+    Simulator simulator(gpuIndex, randomSeed, randomSeed2);
+    simulator.simulate(fleet1, fleet2);
     return 0;
 }
