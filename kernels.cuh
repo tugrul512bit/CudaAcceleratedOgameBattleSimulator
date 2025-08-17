9b293cb61b4cd9b09e10759676f41f15948baa72
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2025-08-17 17:32:04 +0300

    Update kernels.cuh

--- kernels.cuh
+++ kernels.cuh
@@ -15,4 +15,5 @@ struct SpecShipBlockDescriptor {
 namespace Kernels {
 	__global__ void k_initializeRandomSeeds(curandState* seeds, uint32_t* numShips, uint64_t* baseSeed);
 	__global__ void k_initializeShipHulls(uint32_t* numShipsTeam1, uint32_t* numShipsTeam2, SpaceShip* shipsTeam1, SpaceShip* shipsTeam2, SpaceShip* shipsBackupTeam1, SpaceShip* shipsBackupTeam2, SpecShipBlockDescriptor* shipTypesAndNumbersTeam1, SpecShipBlockDescriptor* shipTypesAndNumbersTeam2, uint32_t* numDescriptorsTeam1, uint32_t* numDescriptorsTeam2, uint32_t* defaultShipHulls, uint32_t* numberOfShipTypes);
+	__global__ void k_pickTarget(curandState* seedsTeam1, curandState* seedsTeam2, SpaceShip* shipsTeam1, SpaceShip* shipsTeam2, uint32_t* numShipsTeam1, uint32_t* numShipsTeam2);
 }
